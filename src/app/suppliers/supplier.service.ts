d5a03572654a7ffef966040313aa256bd10fb826
Andres <andrespodadera87@gmail.com>
2024-09-18 21:03:40 +0200

    changing route in prod

--- src/app/suppliers/supplier.service.ts
+++ src/app/suppliers/supplier.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Supplier } from './supplier.dto';
-import { environment } from '../../environments/environment.prod';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
