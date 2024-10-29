263539163089dd6e023fa40385c88c2902b1d959
Andrés Podadera <andrespodadera87@gmail.com>
2024-10-29 21:09:07 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api: 'https://my-sales-app-fake-server.vercel.app/',
+  api: 'https://my-sales-app-fake-server-ondnk9vvu-andres-projects-3b531fe7.vercel.app/',
 };
