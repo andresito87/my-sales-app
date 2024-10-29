b4138e5a40c937b491edbeec1fb3100c9fb81fe2
Andres <andrespodadera87@gmail.com>
2024-10-29 20:43:20 +0100

    updating backend deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api: 'https://my-sales-app-fake-server-2g9fq24pb-andres-projects-3b531fe7.vercel.app/',
+  api: 'https://my-sales-app-fake-server-ondnk9vvu-andres-projects-3b531fe7.vercel.app/',
 };
